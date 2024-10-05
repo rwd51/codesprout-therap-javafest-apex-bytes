6797b218a03f85cdad2f399687b3224ecb5222a6
Abrar Zahin Raihan <raihanzahin95@gmail.com>
2024-10-05 23:39:58 +0600

    pushed

--- src/App.js
+++ src/App.js
@@ -34,8 +34,8 @@ import LevelCompleteScreen from './components/CodeEditorPage/CodeEditor/componen
 
 
 function App() {
-  const [auth, setAuth] = useState(true);
-  const [userType, setUserType] = useState("parents");
+  const [auth, setAuth] = useState(false);
+  const [userType, setUserType] = useState(null);
   const [userID, setUserID] = useState(null);
 
   return (
