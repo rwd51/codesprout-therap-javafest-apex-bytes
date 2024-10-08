3fc4dcb9238f95cf82ef127fee53586db871b6b2
Ruwad Naswan <141907955+rwd51@users.noreply.github.com>
2024-10-08 06:47:10 +0600

    Update env.js

--- src/env.js
+++ src/env.js
@@ -4,6 +4,6 @@ export const PROJECT_SERVICE_URI="https://ada8-103-175-130-28.ngrok-free.app/api
 export const RECOMMENDATION_SERVICE_URI="https://4f5f-103-175-131-28.ngrok-free.app/api/recommendation"
 export const PROBLEMS_SERVICE_URI=" https://abd0-103-175-130-31.ngrok-free.app/api/problems"
 export const OPEN_AI_URI="https://395b-103-175-131-30.ngrok-free.app/api"
-export const CHATBOT_URI="https://ca66-103-175-131-29.ngrok-free.app/api/chat"
+export const CHATBOT_URI="https://4233-103-175-130-28.ngrok-free.app/api/chat"
 
 // export const PROBLEMS_SERVICE_URI="https://8a3b-103-175-131-30.ngrok-free.app/api/problems"
