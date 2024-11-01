8777b15786ef9e2b9fe2225c405b19db6809c92d
Ruwad Naswan <141907955+rwd51@users.noreply.github.com>
2024-11-01 09:34:57 +0600

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -8,7 +8,7 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyA6XW2TpJaqn-cD1IfJKdnIqWZL7Vgnnt4",
+    apiKey:"random123",
     authDomain: "codesprout-frontend.firebaseapp.com",
     projectId: "codesprout-frontend",
     storageBucket: "codesprout-frontend.appspot.com",
